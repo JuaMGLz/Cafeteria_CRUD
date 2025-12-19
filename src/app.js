20fae3c25c80c3c48a56bacabb387a0df6d5c17f
Juan Manuel <jm.ge.esp@gmail.com>
2025-12-19 14:53:08 -0600

    Agregar GitHub Action para desplegar carpeta public

--- src/app.js
+++ src/app.js
@@ -1,15 +1,21 @@
 const express = require('express');
+const cors = require('cors'); 
 require('dotenv').config();
 
 const app = express();
+
+app.use(cors()); 
+
 app.use(express.json());
 
 app.use('/api/productos', require('./routes/productosRoutes'));
 app.use('/api/clientes', require('./routes/clientesRoutes'));
 app.use('/api/ventas', require('./routes/ventasRoutes'));
 
-const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
-app.listen(PORT, () => {
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Servidor iniciado en puerto ${PORT}`);
 });
